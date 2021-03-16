e67294407db222bccce4ebe1a2dedfef5f551e67
Mark <krtlzasn@gmail.com>
2021-03-16 13:29:35 +0300

    Проделана работа над тестами

--- test/TodoApiTests/TodoItemsControllerTest.cs
+++ test/TodoApiTests/TodoItemsControllerTest.cs
@@ -9,17 +9,17 @@ using Microsoft.AspNetCore.Mvc;
 
 namespace TodoApiTests
 {
-    public class UnitTest1
+    public class TodoItemsControllerTest
     {
         private TodoItemsController sut;
         private readonly Mock<ITodoApiRepository> repository = new Mock<ITodoApiRepository>();
-        public UnitTest1()
+        public TodoItemsControllerTest()
         {
             this.sut = new TodoItemsController(this.repository.Object);
         }
         
         [Fact]
-        public void GetAnswerNotNull()
+        public void GetTodoItemsShouldNotNullActionResult()
         {   
             var result = this.sut.GetTodoItems();
             Assert.NotNull(result);
@@ -28,7 +28,7 @@ namespace TodoApiTests
         [InlineData(1)]
         [InlineData(2)]
         [InlineData(3)]
-        public void GetAnswer404(long value)
+        public void GetTodoItemShouldNotFoundStatusCode(long value)
         {
             var result = this.sut.GetTodoItem(value);
             Assert.Equal(404, ((StatusCodeResult)result.Result.Result).StatusCode);
@@ -37,17 +37,27 @@ namespace TodoApiTests
         [InlineData(1)]
         [InlineData(2)]
         [InlineData(3)]
-        public void GetAnswer400(int id)
+        public void PutTodoItemShould400StatusCode(int id)
         {
             TodoItem item = new TodoItem();
             var result = this.sut.PutTodoItem(id, item);
             Assert.Equal(400, ((StatusCodeResult)result.Result).StatusCode);
         }
-        public void GetAnswer415(int id)
+        [Fact]
+        public void PostTodoTtemShould201StatusCode()
         {
-            //TodoItem item = new TodoItem();
-            //var result = this.sut.PostTodoItem();
-            //Assert.Equal(400, ((StatusCodeResult)result.Result).StatusCode);
+            TodoItem item = new TodoItem();
+            var result = this.sut.PostTodoItem(item);
+            Assert.Equal(201, ((ObjectResult)result.Result.Result).StatusCode);
+        }
+        [Theory]
+        [InlineData(1)]
+        [InlineData(2)]
+        [InlineData(3)]
+        public void DeleteTodoitemShould404StatusCode(int id)
+        {
+            var result = this.sut.DeleteTodoItem(id);
+            Assert.Equal(404, ((StatusCodeResult)result.Result).StatusCode);
         }
     }
 }
